125e1abce07f14d7a22e7deebf6856d5d0d3e65c
Jude Dwight <dwightjimenez14@outlook.com>
2025-05-20 21:09:31 +0800

    fix: refactor AuditLog model definition for improved readability

--- server/models/AuditLog.js
+++ server/models/AuditLog.js
@@ -1,35 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  const AuditLog = sequelize.define(
-    "AuditLog",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      action: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER, // e.g., "admin" or "system"
-        allowNull: false,
-        references: { model: "users", key: "id" },
-      },
-      timestamp: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-      },
+  const AuditLog = sequelize.define("AuditLog", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      tableName: "audit_logs",
-      timestamps: false,
-    }
-  );
+    action: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER, // e.g., "admin" or "system"
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
 
   AuditLog.associate = (models) => {
     AuditLog.belongsTo(models.Users, {
