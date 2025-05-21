91182256425bebefb4ee984a4246661906f2302d
Jude Dwight <dwightjimenez14@outlook.com>
2025-05-21 10:13:34 +0800

    fix: update cookie settings for Google login to enhance security and extend expiration

--- server/routes/GoogleAuth.js
+++ server/routes/GoogleAuth.js
@@ -108,9 +108,9 @@ router.post("/google-login", async (req, res) => {
     // Update the cookie with the new token
     res.cookie("token", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
-      maxAge: 3600000,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     // Send the response with user info and the token
